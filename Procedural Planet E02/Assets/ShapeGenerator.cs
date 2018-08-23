ce482fe82e4b6f4e25c247b28dce54deccf84fb7
Sebastian Lague <sebastlague@gmail.com>
2018-08-23 14:06:32 +0200

    Added E02

---
+++ Procedural Planet E02/Assets/ShapeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator {
+
+    ShapeSettings settings;
+
+    public ShapeGenerator(ShapeSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
+    {
+        return pointOnUnitSphere * settings.planetRadius;
+    }
+}
---
+++ Procedural Planet E02/Assets/ShapeSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ShapeSettings : ScriptableObject {
+
+    public float planetRadius = 1;
+}
